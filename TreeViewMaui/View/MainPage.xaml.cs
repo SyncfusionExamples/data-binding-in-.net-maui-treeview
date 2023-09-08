cf34c0189518366d72f474a04e92560470cb9a6c
DhivyaSF4094 <dhivya.sf4094@gmail.com>
2023-09-08 13:17:09 +0530

    BoundMode sample

---
+++ TreeViewMaui/View/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Syncfusion.Maui.TreeView;
+using Syncfusion.TreeView.Engine;
+
+namespace TreeViewMaui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+      
+    }
+}
+
